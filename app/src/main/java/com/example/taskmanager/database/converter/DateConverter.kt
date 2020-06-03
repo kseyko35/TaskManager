a88e2cf251bac2bcf9995a78f1d1dffcacb425c3
kseyko35 <seyfiercan35@hotmail.com>
2020-06-03 12:00:08 +0300

    Added date converter to push date to database

---
+++ app/src/main/java/com/example/taskmanager/database/converter/DateConverter.kt
@@ -0,0 +1,26 @@
+package com.example.taskmanager.database.converter
+
+import androidx.room.TypeConverter
+import java.util.*
+
+
+/**     Code with ❤
+╔════════════════════════════╗
+║   Created by Seyfi ERCAN   ║
+╠════════════════════════════╣
+║  seyfiercan35@hotmail.com  ║
+╠════════════════════════════╣
+║        02,June,2020        ║
+╚════════════════════════════╝
+ */
+class DateConverter {
+    @TypeConverter
+    fun toDate(timestamp: Long?): Date? {
+        return if (timestamp == null) null else Date(timestamp)
+    }
+
+    @TypeConverter
+    fun toTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
